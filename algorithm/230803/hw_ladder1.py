1c3faff1f26e640829ea84fee829ca83fd3b0aba
seungho <kials1@naver.com>
2023-08-03 22:09:53 +0900

    230803

--- algorithm/230803/hw_ladder1.py
+++ algorithm/230803/hw_ladder1.py
@@ -1,32 +1,36 @@
-def is_valid(r,c):
-    return 0 <= r < 100 and 0 <= c < 100
+import sys
 
-T = int(input())
+sys.stdin = open('input.txt', 'r')
+
+
+T = 10
 for tc in range(1, T+1):
+    case_num = int(input())
     matrix = [list(map(int, input().split())) for _ in range(100)]
-    # 아래, 오른쪽, 왼쪽
-    dr = [1,0,0]
-    dc = [0,1,-1]
+    # 도착지에서 출발
+    for goal in range(100):
+        if matrix[99][goal] == 2:
+            c = goal
+    r = 99
+    while r:
+        # 오른쪽
+        if c + 1 < 100 and matrix[r][c+1] == 1 and r != 99:
+            # 못 갈때까지 쭉
+            while True:
+                if 0 <= c + 1 < 100 and matrix[r][c+1] == 1:
+                    c += 1
+                else:
+                    break
+        # 왼쪽
+        elif c - 1 > 0 and matrix[r][c-1] == 1 and r != 99:
+            # 못 갈때까지 쭉
+            while True:
+                if 0 <= c < 100 and matrix[r][c-1] == 1:
+                    c -= 1
+                else:
+                    break
+        # 위로
+        if 0 <= r < 100 and matrix[r-1][c] == 1:
+            r -= 1
 
-    r = 0
-    result = 0
-    for c in range(100):
-        start = c
-        while r < 100:
-            matrix[r][c] = 0
-            rr, rc = r + dr[1], c + dr[1]
-            if is_valid(rr,rc) and (matrix[rr][rc] == 1 or matrix[rr][rc] == 2):
-                r, c = rr, rc    
-                continue        
-            lr, lc = r + dr[2], r + dr[2]
-            if is_valid(lr,lc) and (matrix[lr][lc] == 1 or matrix[lr][lc] == 2):
-                r, c == lr, lc
-                continue
-                
-            sr, sc = r + dr[0], c + dc[0]
-            if is_valid(sr,sc) and (matrix[sr][sc] == 1 or matrix[sr][sc] == 2):
-                r, c = sr, sc
-        if matrix[r][c] == 2:
-            result = start
-    print(f'#{tc}', result)
-
+    print(f'#{tc}', c)
