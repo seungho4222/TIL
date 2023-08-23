f6668ae7d88301b6f557557d76aa031cb0d0042c
miwookkim <minwookkim@naver.com>
2023-08-23 13:55:23 +0900

    230823

--- algorithm/230823/ws_이진수.py
+++ algorithm/230823/ws_이진수.py
@@ -1,3 +1,21 @@
+# T = int(input())
+# for tc in range(1, T+1):
+#     # N: 자리수, hexadecimal: N자리 16진수
+#     N, hexadecimal = input().split()
+#     print(f'#{tc}', end=' ')
+#     # 16진수 각자리 순회
+#     for i in hexadecimal:
+#         # 10진수 변경
+#         decimal = int(i, 16)
+#         # 2진수 변경 후 0b 삭제
+#         binary = bin(decimal)[2:]
+#         # 4자리수 맞춰주기
+#         while len(binary) < 4:
+#             binary = '0' + binary
+#         print(binary, end='')
+#     print()
+
+
 T = int(input())
 for tc in range(1, T+1):
     # N: 자리수, hexadecimal: N자리 16진수
@@ -5,12 +23,8 @@ for tc in range(1, T+1):
     print(f'#{tc}', end=' ')
     # 16진수 각자리 순회
     for i in hexadecimal:
-        # 10진수 변경
-        decimal = int(i, 16)
-        # 2진수 변경 후 0b 삭제
-        binary = bin(decimal)[2:]
-        # 4자리수 맞춰주기
-        while len(binary) < 4:
-            binary = '0' + binary
-        print(binary, end='')
+        # 16진수 ->10진수 -> 2진수 변경 후 0b 삭제
+        binary = bin(int(i, 16))[2:]
+        # 4자리수 맞추기
+        print(f'{int(binary):04d}', end='')
     print()
