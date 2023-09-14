09d7cdeca51af32722086be3790ec9bc63a17fae
seungholee <kials1@naver.com>
2023-09-14 12:20:29 +0900

    230914

--- 04_django/230914/0914_course/articles/models.py
+++ 04_django/230914/0914_course/articles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class Article(models.Model):
@@ -6,4 +7,7 @@ class Article(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
+
+    def __str__(self):
+        return f'{datetime.date}/{}에 생성된 {self.id}번글-{self.title}:{self.content}'
